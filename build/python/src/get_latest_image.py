cf550ee795750e72dd8caa531da69a6e9abc73bf
Calvin Carter <calvincarter@pingidentity.com>
2022-06-01 12:38:13 -0500

    Refactor get_latest_image.py to get latest image within v#.# release

--- build/python/src/get_latest_image.py
+++ build/python/src/get_latest_image.py
@@ -5,93 +5,99 @@ import re as regex
 import json
 import logging
 
-class Latest_Image_Manager:
-  "Get the latest release candidate ECR image"
+# Constants
+SEMANTIC_VERSION_REGEX = "([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"
 
-  def __init__(self, ORIG_GITLAB_TAG_NAME, REPOSITORY_NAME):
+class Latest_Image_Manager:
+  "Get the latest ECR image"
+
+  def __init__(self, orig_gitlab_name, repository_name):
     """
       Create initial configuration by connecting to public ECR.
 
       Arguments
       ----------
-      GITLAB_TAG_NAME: string
+      orig_gitlab_name: string
         Gitlab tag name created in ping-cloud-docker repository
-      REPOSITORY_NAME: string
+      repository_name: string
         Location of image
     """
 
-    self.SEMANTIC_VERSION_REGEX = "([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"
-    self.AWS_ACCOUNT_ID='705370621539'
-    self.ORIG_GITLAB_TAG_NAME = ORIG_GITLAB_TAG_NAME
-    self.REPOSITORY_NAME = REPOSITORY_NAME
+    self.orig_gitlab_name = orig_gitlab_name
+    self.repository_name = repository_name
 
     # Extract infrastructure version | beluga major version |  ping-cloud-base patch | ping-cloud-docker patch from Gitlab tag
-    self.INFRASTRUCTURE_VERSION_NUM, \
-    self.BELUGA_MAJOR_VERSION_NUM=self.normalize_gitlab_tag()
+    self.infrastructure_version_num, \
+    self.beluga_major_version_num=self.normalize_gitlab_tag()
 
+    # ECR public only works against us-east-1
     boto_session = utils.get_boto_session()
-    # ECR public describe images only works against us-east-1
     config = Config(region_name="us-east-1")
     self.client = boto_session.client("ecr-public", config=config)
 
 
-  def regex_for_release_candidate_within_specific_release(self):
-    return f"({self.INFRASTRUCTURE_VERSION_NUM})\.({self.BELUGA_MAJOR_VERSION_NUM})\.([0-9]+)\.([0-9]+)_CALVIN_TEST$"
+  def regex_for_image_within_specific_release(self):
+    return f"({self.infrastructure_version_num})\.({self.beluga_major_version_num})\.([0-9]+)\.([0-9]+)_CALVIN_TEST$"
 
   def normalize_gitlab_tag(self):
-    gitlab_tag_name = regex.search(self.SEMANTIC_VERSION_REGEX, self.ORIG_GITLAB_TAG_NAME)
+    """
+      Extract infrastructure version | beluga major version from gitlab tag name.
+    """
+    gitlab_tag_name = regex.search(SEMANTIC_VERSION_REGEX, self.orig_gitlab_name)
 
     if gitlab_tag_name is None:
-      raise Exception(f"Unexpected Results: Invalid Gitlab tag name - {self.ORIG_GITLAB_TAG_NAME}")
+      raise Exception(f"Unexpected Results: Invalid Gitlab tag name - {self.orig_gitlab_name}")
 
-    # Only retrieve pattern #.#.#.#
+    # Only retrieve pattern #.#.#.#, but only extract the infrastructure and major version.
     gitlab_infrastructure_version_num = int(gitlab_tag_name.group(1))
     gitlab_major_version_num = int(gitlab_tag_name.group(2))
 
-
-    # Return integers: infrastructure version | beluga major version |  ping-cloud-base patch | ping-cloud-docker patch)
+    # Return integers: infrastructure version | beluga major version
     return [gitlab_infrastructure_version_num, gitlab_major_version_num]
 
   def get_all_images_in_detail(self):
     """
-      Get image detail information within ECR.
+      Get all images within ECR.
 
       API Resource:
         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecr-public.html#ECRPublic.Client.describe_image_tags
-
-      FIXME:
-        Consider pagination, if for some reason we decide to support over 1000 images in production for each ECR image
     """
-    return self.client.describe_image_tags(
-      registryId=self.AWS_ACCOUNT_ID,
-      repositoryName=self.REPOSITORY_NAME,
+    all_images = []
+
+    # Make initial API call to get first 1000 images
+    response = self.client.describe_image_tags(
+      repositoryName=self.repository_name,
       maxResults=1000
-    ).get('imageTagDetails')
+    )
+    all_images += response.get('imageTagDetails')
+
+    # If there are more than 1000 images, paginate and retrieve the others
+    while "nextToken" in response:
+      response = self.client.describe_image_tags(
+        repositoryName=self.repository_name,
+        maxResults=1000,
+        nextToken=response["nextToken"]
+      )
+      all_images += response.get('imageTagDetails')
+
+    return all_images
 
 
   def get_latest_image(self):
     """
-      Filter out all release candidate images are in the same release (infrastructure_version and beluga_major_version).
-      Return the most recent release candidate image that matches the gitlab tag name.
-
-      If no image matches gitlab tag, then function will return the most recent release candidate.
+      Filter out all images that are in the same release (infrastructure_version and beluga_major_version).
+      Return the most recent image for the given product.
     """
     all_images_within_release = []
     for image in self.get_all_images_in_detail():
       orig_image_tag_name = image.get('imageTag')
 
       if orig_image_tag_name is not None:
-        image_tag_name = regex.search(self.regex_for_release_candidate_within_specific_release(), orig_image_tag_name)
-
-        
+        image_tag_name = regex.search(self.regex_for_image_within_specific_release(), orig_image_tag_name)
 
         if image_tag_name is not None:
-          # print(image_tag_name.group(0))
-          # if 'RC' in image_tag_name.group(0):
-          #   # skip release candidate
-          #   continue
 
-          # Extract semantic version and RC number into a list as "version_list"
+          # Extract semantic version into a list as "version_list"
           # e.g.
           # image_tag_name => v1.14.0.0
           # image_version_list  => [1, 14, 0, 0]
@@ -100,7 +106,7 @@ class Latest_Image_Manager:
           image_beluga_major_version_num = int(image_tag_name.group(2))
           image_ping_cloud_base_patch = int(image_tag_name.group(3))
           image_ping_cloud_docker_patch = int(image_tag_name.group(4))
-        
+
 
           image_version_list = [
                                 image_infrastructure_version_num,
@@ -111,24 +117,22 @@ class Latest_Image_Manager:
 
           all_images_within_release.append({
             "image_version_list": image_version_list,
-            "image_tag_name":orig_image_tag_name
+            "image_tag_name": orig_image_tag_name
           })
 
     if len(all_images_within_release) == 0:
-      raise Exception(f"No image was found within {self.INFRASTRUCTURE_VERSION_NUM}.{self.BELUGA_MAJOR_VERSION_NUM} release")
+      raise Exception(f"No image was found within {self.infrastructure_version_num}.{self.beluga_major_version_num} release")
 
     # Sort release candidates by highest to lowest. The highest RC is considered as the most recent.
     all_images_within_release.sort(key=lambda version: version["image_version_list"], reverse=True)
 
-    # Default highest release candidate, the default is needed when there is a PING_CLOUD_BASE_PATCH
-    highest_release_candidate = all_images_within_release[0]
-
-    return highest_release_candidate["image_tag_name"]
+    # Return the first item from the list. This is the latest release candidate within the release.
+    return all_images_within_release[0]["image_tag_name"]
 
 if __name__ == '__main__':
     # repo_name = sys.argv[1]
     # tag = sys.argv[2]
 
     # lim = Latest_Image_Manager(tag, repo_name)
-    lim = Latest_Image_Manager("v1.14.0.0", "pingcloud-apps/pingaccess")
+    lim = Latest_Image_Manager("v1.14.0.0", "pingcloud-apps/pingcentral")
     print(lim.get_latest_image())
