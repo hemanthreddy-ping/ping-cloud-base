19f3d85c62ed325359f8d68e830864d65392bf71
Hemanth Reddy <hemanthreddy@pingidentity.com>
2022-07-08 18:48:30 +0000

    Update ci-scripts/test/integration/pingone/test_pingoneconfigurator.py

--- ci-scripts/test/integration/pingone/test_pingoneconfigurator.py
+++ ci-scripts/test/integration/pingone/test_pingoneconfigurator.py
@@ -9,7 +9,25 @@ class TestPingOneConfigurator(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         config.load_kube_config()
-        cls.core_client = client.CoreV1Api()       
+        cls.core_client = client.CoreV1Api()
+
+    def setUp(self):
+        self.endpoint = self.get_pingoneconfigurator_endpoint()
+
+    def get_pingoneconfigurator_endpoint(self) -> str:
+        response = self.network_client.list_ingress_for_all_namespaces(
+            _preload_content=False
+        )
+        routes = json.loads(response.data)
+        hostname = next(
+            (
+                route["spec"]["rules"][0]["host"]
+                for route in routes["items"]
+                if "pingoneconfigurator" in route["spec"]["rules"][0]["host"]
+            ),
+            None,
+        )
+        return f"http://{hostname}"       
 
     def test_pingoneconfigurator_pod_exists(self):
         pods = self.core_client.list_pod_for_all_namespaces(watch=False)
@@ -22,6 +40,10 @@ class TestPingOneConfigurator(unittest.TestCase):
             False,
         )
         self.assertTrue(res)
+    
+    def test_pingoneconfigurator_get_route_ok_response(self):
+        res = requests.get(self.endpoint, verify=False)
+        self.assertEqual(200, res.status_code)
         
 if __name__ == "__main__":
     unittest.main()
