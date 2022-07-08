1d1bc392e5492acdfab8d5c7148c9ab3b1f3572c
Hemanth Reddy <hemanthreddy@pingidentity.com>
2022-07-08 15:19:33 +0000

    Update ci-scripts/test/integration/pingone/test_pingoneconfigurator.py

--- ci-scripts/test/integration/pingone/test_pingoneconfigurator.py
+++ ci-scripts/test/integration/pingone/test_pingoneconfigurator.py
@@ -5,7 +5,7 @@ import requests
 from kubernetes import client, config
 
 
-class TestHealthcheck(unittest.TestCase):
+class TestPingOneConfigurator(unittest.TestCase):
     core_client = None
 
     @classmethod
@@ -15,24 +15,9 @@ class TestHealthcheck(unittest.TestCase):
         cls.network_client = client.NetworkingV1Api()
 
     def setUp(self):
-        self.endpoint = self.get_healthcheck_endpoint()
+        self.endpoint = self.get_pingoneconfigurator_endpoint()
 
-    def get_healthcheck_endpoint(self) -> str:
-        response = self.network_client.list_ingress_for_all_namespaces(
-            _preload_content=False
-        )
-        routes = json.loads(response.data)
-        hostname = next(
-            (
-                route["spec"]["rules"][0]["host"]
-                for route in routes["items"]
-                if "healthcheck" in route["spec"]["rules"][0]["host"]
-            ),
-            None,
-        )
-        return f"http://{hostname}"
-
-    def test_healthcheck_pod_exists(self):
+    def test_pingoneconfigurator_pod_exists(self):
         pods = self.core_client.list_pod_for_all_namespaces(watch=False)
         res = next(
             (
@@ -43,11 +28,6 @@ class TestHealthcheck(unittest.TestCase):
             False,
         )
         self.assertTrue(res)
-
-    def test_healthcheck_get_route_ok_response(self):
-        res = requests.get(self.endpoint, verify=False)
-        self.assertEqual(200, res.status_code)
-
-
+        
 if __name__ == "__main__":
     unittest.main()
