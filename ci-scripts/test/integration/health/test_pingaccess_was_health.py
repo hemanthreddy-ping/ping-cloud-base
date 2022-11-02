1bd282323a156ec896613e28816d94f75bb70ae1
Colin Reininger <colinreininger@pingidentity.com>
2022-11-02 15:53:26 -0600

    Update integration tests for refactored test names

--- ci-scripts/test/integration/health/test_pingaccess_was_health.py
+++ ci-scripts/test/integration/health/test_pingaccess_was_health.py
@@ -50,21 +50,21 @@ class TestPingAccessWASHealth(TestHealthBase):
 
     def test_health_check_has_create_object_results(self):
         test_results = self.get_test_results(
-            self.pingaccess_was, Categories.pod_status
+            self.pingaccess_was, Categories.connectivity
         )
-        res = [key for key in test_results if "An object can be created in PA-WAS Admin" in key]
+        res = [key for key in test_results if "create an object" in key]
         self.assertTrue(
             len(res) > 0,
-            "No 'An object can be created in PA-WAS Admin' checks found in health check results",
+            "No 'create an object' checks found in health check results",
         )
 
     def test_health_check_has_proxy_results(self):
         test_results = self.get_test_results(
             self.pingaccess_was, Categories.connectivity
         )
-        res = [key for key in test_results if "Can PA-WAS proxy an unauthenticated request" in key]
+        res = [key for key in test_results if "proxy an unauthenticated request" in key]
         self.assertTrue(
             len(res) > 0,
-            "No 'Can PA-WAS proxy an unauthenticated request' checks found in health check results",
+            "No 'proxy an unauthenticated request' checks found in health check results",
         )
 
